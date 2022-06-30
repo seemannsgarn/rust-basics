6cda724720fcbe563fee952abdf22f8587934a6e
musta700@gmail.com <musta700@gmail.com>
2022-06-30 09:19:25 +0300

    upd 30.06.2022

---
+++ variables/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    
+    let x = 45;         // by default in rust all variables is immutable
+    let mut y = 10;     // this is define the mutable variable
+    println!("The value of x is {}", x);
+    y = 75;
+    println!("The mutable value of y is {}", y)
+
+    let a = 33;         // i32
+    let b: i64 = 33;    // i64
+    let c: u64 = 45;    // u64 unsigned data type
+    let d: f32 = 3.2    // f32 float
+    let f: bool = true  // boolean
+
+    
+
+}
